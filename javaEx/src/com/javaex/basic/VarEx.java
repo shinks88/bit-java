3a43704ae09afbe0f7b4c2c39c282cfd3219741d
shinks <shinks8888@gmail.com>
2020-08-04 12:13:16 +0900

    commit

---
+++ javaEx/src/com/javaex/basic/VarEx.java
@@ -0,0 +1,33 @@
+package com.javaex.basic;
+
+public class VarEx {
+		/**
+		 * @param args
+		 */
+		public static void main(String[] args) {
+			// 변수 식별자
+			// 문자, 숫자, $,_ 의 조합
+			// 다만, 숫자로 시작하면 안됨
+			
+			/*
+			 * 변수명 관례
+			 * 	- 변수명은 소문자로 작성
+			 *  - 단어의 조합일 경우, 두번째 단어는 대문자로 시작
+			 */
+			// 선언
+			int myAge; // myAge라는 이름의 int(4byte) 변수 공간을 만든다
+			// 초기화
+			myAge = 29;
+			
+			int myAge2 = 29; 	//선언과 초기화를 동시에 할 수 있다(추천)
+			
+			//	변수의 경우 선언시 지정한 데이터 타입 이외ㅢ 데이터는 넣을 수 없다
+//			myAge = 12345678012234567890; // error
+			
+			//whghldhk qusrud
+			System.out.println("myAge의 내용:"+ myAge);
+			myAge = myAge + 1;
+			System.out.println("변경된 myAge의 내용:"+ myAge);
+		}
+
+}
---
+++ javaEx/src/com/javaex/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.javaex.helloworld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		
+		
+	}
+}
