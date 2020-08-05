43e71bd682cb538d8e0928f350ecac1ee3919235
Shin <shinks8888@gmail.com>
2020-08-05 11:00:35 +0900

    commit

---
+++ javaEx/src/com/javaex/basic/TypeEx.java
@@ -0,0 +1,65 @@
+package com.javaex.basic;
+
+public class TypeEx {
+	
+	//	자바의 기본 자료형 연습
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		intLongTest();
+		floatDoubleTest();
+	}
+	
+	// 실수형 연습
+	// float(4) < double(8)
+	public static void floatDoubleTest() {
+		// 정밀도를 포기, 표현범위를 넓힌 자료형
+		float fVar = 3.14159F; // float는 마지막에 F
+		double dVar = 3.14159;
+		
+		// 정밀도 파악
+		fVar = 0.1234567890123456789F;
+		dVar = 0.1234567890123456789;
+		
+		System.out.println("float:"+ fVar);
+		System.out.println("double:"+ dVar);
+		
+		// 지수 표기법
+		fVar = 1234567890E-10F;
+		System.out.println(fVar);
+		
+		// 실수 자료형의 처리는 정밀도가 떨어진다(표현 범위를 넓힘)
+		System.out.println(0.1*3);
+	}
+	
+	// 정수형 연습
+	// byte < short < int < long
+	public static void intLongTest() {
+		// 변수 선언
+		int intVar1;
+		intVar1 = 2020; //초기화
+		int intVar2 = 2020; // 선언+초기화
+		System.out.println(intVar1);
+		System.out.println(intVar2);
+		
+		//	long : 8Byte
+		long longVar1;
+		longVar1 = 2020;
+		long longVar2 = 1234567812345678L;  // 맨 뒤에 L
+		
+		System.out.println(longVar1);
+		System.out.println(longVar2);
+		
+		// 2진수, 8진수, 16진수
+		int bin = 0b1100; // 2진수 0b로 시작
+		int oct = 010; // 8진수는 0으로 시작
+		int hex = 0xFF; // 16진수는 0x로 시작
+		
+		System.out.println(bin);
+		System.out.println(oct);
+		System.out.println(hex);
+		
+		
+	}
+	
+}
