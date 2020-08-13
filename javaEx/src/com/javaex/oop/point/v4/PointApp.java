3f085598a3f91638ebb1c4fa79b46a129602dec2
shink <shink@DESKTOP-8B2MLUS>
2020-08-13 12:49:06 +0900

    상속

---
+++ javaEx/src/com/javaex/oop/point/v4/PointApp.java
@@ -0,0 +1,32 @@
+package com.javaex.oop.point.v4;
+
+public class PointApp {
+
+	public static void main(String[] args) {
+	
+		Point a = new Point(); // 기본 생성자 사용
+		 
+		Point b = new Point(10, 23); // 사용자 정의 생서자 사용
+		
+		a.setX(5);
+		a.setY(5);
+		
+		//b.setX(10);
+		//b.setY(23);
+		
+		a.draw();
+		b.draw();
+
+		// 오버로딩된 메서드
+		a.draw(false);
+		b.draw(false);
+		
+		//	자식 클래스 생성
+		ColorPoint cp = new ColorPoint("red");
+		ColorPoint cp2 = 
+				new ColorPoint(10, 20, "blue");
+		cp.draw(true);
+		cp2.draw(true);
+	}
+
+}
---
+++ javaEx/src/com/javaex/oop/staticmembers/SingletonApp.java
@@ -0,0 +1,20 @@
+package com.javaex.oop.staticmembers;
+
+public class SingletonApp {
+
+	public static void main(String[] args) {
+//		Singleton s1 = new Singleton();	
+		// 생성자가 private이라 new가 안된다
+		Singleton s1 = Singleton.getInstance();
+		Singleton s2 = Singleton.getInstance();
+		
+		//	s1의 주소와 s2의 주소가 같은가?
+		System.out.println("s1 = " + s1);
+		System.out.println("s2 = " + s2);
+		
+		//	주소의 비교 ==
+		System.out.println("s1과 s2는 같은 객체?" + (s1==s2));
+
+	}
+
+}
